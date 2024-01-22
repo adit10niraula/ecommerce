6710b62c2dd862ac8b07efcb412dcad2e0f8cc9c
raj kumar <razzkumar115@gmail.com>
2024-01-22 09:00:02 +0545

    models added

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Order,Customer,OrderItem,Product,ShippingAddress
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Product)
+admin.site.register(ShippingAddress)
+admin.site.register(Customer)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user
+    
+class Product(models.Model):
+    name= models.CharField(max_length=200)
+    price= models.PositiveIntegerField(null=True, blank=True)
+    digital=models.BooleanField(default=False)
+    image= models.ImageField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Model):
+    customer= models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date_ordered =models.DateTimeField(auto_now_add=True)
+    complete= models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200, null=True, blank=True)
+
+    
+
+
+
+
+class OrderItem(models.Model):
+    product= models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity= models.PositiveIntegerField()
+    date_added= models.DateTimeField(auto_now_add=True)
+
+
+
+
+class ShippingAddress(models.Model):
+    customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order=models.ForeignKey(Order, on_delete=models.CASCADE)
+    address=models.CharField(max_length=200)
+    city=models.CharField(max_length=200)
+    state=models.CharField(max_length=200)
+    zipcode=models.CharField(max_length=200)
+    date_added=models.DateTimeField(auto_now_add=True)
